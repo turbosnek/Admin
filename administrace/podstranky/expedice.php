4a50e2768e3ea8414349614d4dd1ab6c7cd84e63
turbosnek <havlicekl@seznam.cz>
2022-09-19 18:32:04 +0200

    Pracovni pozice
    Výpis zaměstnanců podle pracovní pozice

---
+++ administrace/podstranky/expedice.php
@@ -0,0 +1,46 @@
+<?php
+    if (!isset($_SESSION['uzivatel_id']))
+    {
+        header('Location: ../index.php'); // Pokud není uživatel přihlášenej, přesměrujeme ho na přihlášení
+        exit();
+    }
+
+    if (isset($_GET['odhlasit']))
+    {
+        session_destroy();
+        header('Location: ../index.php'); // Po odhlášení přesměrujeme na přihlášení
+        exit();
+    }
+    
+    /**
+     * Vybereme všechny záznamy z tabulky "zamestnanci" podle jejich pracovní pozice "Expedice"
+     */
+     $zamestnanci = Db::queryAll('
+             SELECT *
+             FROM zamestnanci
+             WHERE pracovni_pozice="Expedice"
+             ORDER BY zamestnanci_id
+             ');
+?>
+
+
+<H1>Výpis zaměstnanců firmy na pozici "retrakář"</H1>
+
+<?php
+     echo('<table id="zamestnanci">');
+     echo('<tr>');
+     echo('<td><center><strong>Jméno</strong></center></td>');
+     echo('</tr>');
+     
+     /**
+      * Pomocí cyklu foreach vypíšeme zaměstnance firmy na pozici expedice
+      */
+     foreach ($zamestnanci as $zamestnanec)
+     {
+         echo('<tr><td><center>
+                 <a href="index.php?stranka=detail&zamestnanci_id=' . htmlspecialchars($zamestnanec['zamestnanci_id']) . '">
+                     ' . htmlspecialchars($zamestnanec['jmeno']) . ' ' . htmlspecialchars($zamestnanec['prijmeni']) .
+             '</a></center></td></tr>');
+     }
+     echo('</table>');
+?>
--- administrace/podstranky/vychystavac.php
+++ administrace/podstranky/vychystavac.php
@@ -1,36 +1,65 @@
-<?php
-    if (!isset($_SESSION['uzivatel_id']))
-    {
-        header('Location: ../index.php'); // Pokud není uživatel přihlášenej, přesměrujeme ho na přihlášení
-        exit();
-    }
+    <?php
+        if (!isset($_SESSION['uzivatel_id']))
+        {
+            header('Location: ../index.php'); // Pokud není uživatel přihlášenej, přesměrujeme ho na přihlášení
+            exit();
+        }
 
-    if (isset($_GET['odhlasit']))
-    {
-        session_destroy();
-        header('Location: ../index.php'); // Po odhlášení přesměrujeme na přihlášení
-        exit();
-    }
-    
-    /**
-     * Vybereme všechny záznamy z tabulky "zamestnanci" podle jejich pracovní pozice "Vychystávač"
-     */
-     $zamestnanci = Db::queryAll('
-             SELECT *
-             FROM zamestnanci
-             ORDER BY pracovni_pozice="Vychystávač"
-             ');
-?>
+        if (isset($_GET['odhlasit']))
+        {
+            session_destroy();
+            header('Location: ../index.php'); // Po odhlášení přesměrujeme na přihlášení
+            exit();
+        }
+
+        /**
+         * Vybereme všechny záznamy z tabulky "zamestnanci" podle jejich pracovní pozice "Vychystávač"
+         */
+         $zamestnanci = Db::queryAll('
+                 SELECT *
+                 FROM zamestnanci
+                 WHERE pracovni_pozice="Vychystávač"
+                 ORDER BY zamestnanci_id
+                 ');
+    ?>
 
 
-<H1>Výpis zaměstnanců firmy na pozici "vychystávač"</H1>
+    <H1>Výpis zaměstnanců firmy na pozici "vychystávač"</H1>
 
-<?php
-    foreach ($zamestnanci as $zamestnanec)
+    <?php
+         echo('<table id="zamestnanci">');
+         echo('<tr>');
+         echo('<td><center><strong>Jméno</strong></center></td>');
+         echo('</tr>');
+
+         /**
+          * Pomocí cyklu foreach vypíšeme zaměstnance firmy na pozici vychystávač
+          */
+         foreach ($zamestnanci as $zamestnanec)
          {
-             echo('
+             echo('<tr><td><center>
                      <a href="index.php?stranka=detail&zamestnanci_id=' . htmlspecialchars($zamestnanec['zamestnanci_id']) . '">
                          ' . htmlspecialchars($zamestnanec['jmeno']) . ' ' . htmlspecialchars($zamestnanec['prijmeni']) .
-                         '</a>');
+                 '</a></center></td></tr>');
          }
-?>
+         echo('</table>');
+    ?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
